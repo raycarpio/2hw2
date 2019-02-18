1138103b95313113ccb61003cc6bbc0fd603d17f
Ray <carpio.ray@gmail.com>
2019-02-17 17:32:51 -0800

    added index.html page

--- buildsite.py
+++ buildsite.py
@@ -1,6 +1,17 @@
 print('Building Site')
 
+#INDEX
+#----------------------------------------------------------------
+top_html = open('templates/top.html').read()
+middle_html = open('content/index.html').read()
+bottom_html = open('templates/bottom.html').read()
+
+combined_html = top_html + middle_html + bottom_html
+open('docs/index.html', 'w+').write(combined_html)
+#----------------------------------------------------------------
+
 #BLOG
+#----------------------------------------------------------------
 top_html = open('templates/top.html').read()
 middle_html = open('content/blog.html').read()
 bottom_html = open('templates/bottom.html').read()
@@ -8,8 +19,10 @@ bottom_html = open('templates/bottom.html').read()
 combined_html = top_html + middle_html + bottom_html
 
 open('docs/blog.html', 'w+').write(combined_html)
+#----------------------------------------------------------------
 
 #PROJECTS
+#----------------------------------------------------------------
 top_html = open('templates/top.html').read()
 middle_html = open('content/projects.html').read()
 bottom_html = open('templates/bottom.html').read()
@@ -17,8 +30,11 @@ bottom_html = open('templates/bottom.html').read()
 combined_html = top_html + middle_html + bottom_html
 
 open('docs/projects.html', 'w+').write(combined_html)
+#----------------------------------------------------------------
+
 
 #CONTACT ME
+#----------------------------------------------------------------
 top_html = open('templates/top.html').read()
 middle_html = open('content/contactme.html').read()
 bottom_html = open('templates/bottom.html').read()
@@ -26,5 +42,6 @@ bottom_html = open('templates/bottom.html').read()
 combined_html = top_html + middle_html + bottom_html
 
 open('docs/contactme.html', 'w+').write(combined_html)
+#----------------------------------------------------------------
 
-print('Finished combining HTML files')
+print('Woot! Finished combining HTML files')
