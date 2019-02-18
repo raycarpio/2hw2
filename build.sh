e6601520cfbbc6c3648969a0d27b52633960f048
Ray <carpio.ray@gmail.com>
2019-02-17 16:48:19 -0800

    adding all files to ssg-branch

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/contactme.html templates/bottom.html > docs/contactme.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
